ec17f8870c15e5de833eb7641e8d9955cc36c117
Hampus Eriksson <hamer848@student.liu.se>
2016-09-18 01:02:15 +0200

    Added small comments and code mods.

--- lab2/wordchain/src/wordchain.cpp
+++ lab2/wordchain/src/wordchain.cpp
@@ -8,13 +8,17 @@ using namespace std;
 
 const string ALPHABET  = "abcdefghijklmnopqrstuvwxyz";
 
-void findNeighbours(stack<string> &stackToCheck, queue<stack<string>> &wordQueueOrg, map<string, string> &dictionary){
+void findNeighbours(stack<string> &stackToCheck, queue<stack<string>> &wordQueueOrg,
+                    map<string, string> &dictionary, map<string, string> &checkedNeighbours){
     string topWord = stackToCheck.top();
     for(char& c : topWord){
         for(const char& alp : ALPHABET){
             string possibleNeighbour = topWord;
-            possibleNeighbour.replace(topWord.find(c), 1, alp);
-            if(dictionary.find(possibleNeighbour)){
+            possibleNeighbour[charNum] = alp;
+            //How to use charNum as index? .size and .lenght does not seem to work.
+            if(dictionary.find(possibleNeighbour) && !checkedNeighbours.find(possibleNeighbour)){
+                //NOTE neither of the operations used in the if returns a bool. Needs to be fixed.
+                //Essentially want to make sure the word is a valid word AND that it hasn't been used.
                 stack<string> stackCpy = stackToCheck;
                 stackCpy.push(possibleNeighbour);
                 wordQueueOrg.push(stackCpy);
@@ -29,25 +33,27 @@ void wordChain(string w1, string w2){
     stack<string> startingWord;
     startingWord.push(w1);
     wordQueue.push(startingWord);
+    map<string, string> checkedNeighbours;
+    checkedNeighbours.insert(w1, w1);
 
     ifstream input;
     input.open("dictionary.txt");
     string wordFromDict;
     map<string, string> dictionary;
 
-    while(getline(input, wordFromDict)){
+    while(getline(input, wordFromDict)){ //Read each line from dictionary.
         dictionary.insert(wordFromDict, wordFromDict);
     }
     input.close();
 
-    while(!wordQueue.empty()){
+    while(!wordQueue.empty()){ //Main loop
         stack<string> stackToCheck = wordQueue.front();
         wordQueue.pop();
         if(stackToCheck.top() == w2){
             //print stack with complete chain
         }
         else {
-            findNeighbours(stackToCheck, wordQueue, dictionary);
+            findNeighbours(stackToCheck, wordQueue, dictionary, checkedNeighbours);
         }
 
     }
