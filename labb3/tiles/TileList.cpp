7f2d7bdf58fcae721c203e58ee279ac674a2b30c
Sebgg <sebastian.grunditz@gmail.com>
2016-10-12 16:34:15 +0000

    Fixed lab 3 part 1!!!!!!!!!!!

--- labb3/tiles/TileList.cpp
+++ labb3/tiles/TileList.cpp
@@ -1,3 +1,4 @@
+
 // This is the .cpp file you will edit and turn in.
 // We have provided a skeleton for you,
 // but you must finish it as described in the spec.
@@ -9,16 +10,7 @@ using namespace std;
 
 TileList::TileList()
 {
-    /*I was thinking, should we create a pointer to an array filled with pointers?
-    * This would mean we could point each pointer to a tile object and when removing
-    * we would simply set pointer to nullptr!
-    *
-    * To create a pointer to a pointer we simply use '**' in front of what should
-    * should be interpreted as a pointer to another pointer:
-    *
-    * Tile **array = new Tile*[capacity]
-    */
-    tileList = new array<Tile, capacity>;
+    tileList = new Tile[capacity];
 }
 
 TileList::~TileList()
@@ -28,21 +20,23 @@ TileList::~TileList()
 
 void TileList::addTile(Tile tile)
 {
-    tileList->at(tileList->size()) = tile;
+    checkResize();
+    tileList[size] = tile;
+    size++;
 }
 
 void TileList::drawAll(QGraphicsScene* scene)
 {
-    for(unsigned int i = 0; i < tileList->size()-1; i++) {
-        Tile tile = tileList->at(i);
+    for(unsigned int i = 0; i <= size; i++) {
+        Tile tile = tileList[i];
         tile.draw(scene);
     }
 }
 
 int TileList::indexOfTopTile(int x, int y)
 {
-    for(int i = tileList->size()-1; i >= 0; i--){
-        Tile temp = tileList->at(i);
+    for(int i = size; i >= 0; i--){
+        Tile temp = tileList[i];
         if(temp.contains(x, y)){
             return i;
         }
@@ -54,11 +48,11 @@ void TileList::raise(int x, int y)
 {
     int index = indexOfTopTile(x, y);
     if(index != -1){
-        Tile found = tileList->at(index);
-        for(unsigned int i = index+1; i < tileList->size(); i++){
+        Tile found = tileList[index];
+        for(unsigned int i = index+1; i <= size; i++){
             tileList[i-1] = tileList[i];
         }
-        tileList->back() = found;
+        tileList[size] = found;
     }
 }
 
@@ -66,28 +60,41 @@ void TileList::lower(int x, int y)
 {
     int index = indexOfTopTile(x, y);
     if(index != -1){
-        Tile found = tileList->at(index);
+        Tile found = tileList[index];
         for(int i = index; i > 0; i--){
             tileList[i] = tileList[i-1];
         }
-        tileList->front() = found;
+        tileList[0] = found;
     }
 }
 
 void TileList::remove(int x, int y)
 {
     int index = indexOfTopTile(x, y);
-    if(index != -1 && index != tileList->size()-1){
-        for(unsigned int i = index+1; i < tileList->size(); i++){
+    if(index != -1 && index != size){
+        for(unsigned int i = index+1; i <= size; i++){
             tileList[i-1] = tileList[i];
         }
-        tileList[(tileList->size())-1] = nullptr;
     }
+    size--;
 }
 
 void TileList::removeAll(int x, int y)
 {
     while(indexOfTopTile(x, y) != -1){
         this->remove(x, y);
+    }
+}
+
+void TileList::checkResize()
+{
+    if(size == capacity){
+        Tile* bigger = new Tile[capacity*2];
+        for(int i = 0; i < size; i++){
+            bigger[i] = tileList[i];
+        }
+        delete[] tileList;
+        tileList = bigger;
+        capacity = capacity*2;
     }
 }
--- labb3/tiles/TileList.h
+++ labb3/tiles/TileList.h
@@ -1,9 +1,3 @@
-// This is the .h file you will edit and turn in.
-// We have provided a skeleton for you,
-// but you must finish it as described in the spec.
-// Also remove these comments here and add your own, as well as on the members.
-// TODO: remove this comment header
-
 #ifndef TILELIST_H
 #define TILELIST_H
 
@@ -21,12 +15,13 @@ public:
     void raise(int x, int y);
     void remove(int x, int y);
     void removeAll(int x, int y);
-    array<Tile, 10> *tileList;
+
 
 private:
-
-
-
+    int capacity = 10;
+    int size = 0;
+    Tile *tileList = new Tile[capacity];
+    void checkResize();
 };
 
 #endif // TILELIST_H
