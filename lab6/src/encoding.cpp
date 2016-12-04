b38dc7081efcb80a6ddecd0ac38611ed847e195d
Sebgg <sebastian.grunditz@gmail.com>
2016-12-04 13:32:40 +0100

    Waow

--- lab6/src/encoding.cpp
+++ lab6/src/encoding.cpp
@@ -99,25 +99,25 @@ void decodeData(ibitstream& input, HuffmanNode* encodingTree, ostream& output) {
     // Take ze decoded bitstream and follow the 1's n' 0's until you reach a character
     // Then Put that char in output. Simple!
     int bitt;
-    string decodeStr; // May not be needed. We could just cout the chars as they're found.
+    string decodedStr; // May not be needed. We could just cout the chars as they're found.
     HuffmanNode* root = encodingTree;
     HuffmanNode *currNode = root;// Would want this to be root. Starting point.
-    while(input.readBit(bitt){
+    while(bitt == input.readBit()){
         if(bitt == 1){
             currNode = currNode->one; // Change currNode to right child of itself.
             if(currNode->isLeaf()){
-                decodedStr += currNode.character;
+                decodedStr += currNode->character;
                 currNode = root;
             }       
         }else if(bitt == 0){
             currNode = currNode->zero; // Change currNode to its left child.
             if(currNode->isLeaf()){
-                decodeStr += currNode->character;
+                decodedStr += currNode->character;
                 currNode = root;
             }
         }
     }
-    cout << decodeStr << endl;
+    cout << decodedStr << endl;
 }
 
 void compress(istream& input, obitstream& output) {
