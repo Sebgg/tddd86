0d4f25ee16b1088079f58ccf3bdd4e7fef8458ec
Hampus Eriksson <hamer848@parlomba2.ida.liu.se>
2016-12-07 17:03:17 +0100

    djikstra tried

--- lab8/src/trailblazer.cpp
+++ lab8/src/trailblazer.cpp
@@ -5,11 +5,25 @@
 
 #include "costs.h"
 #include "trailblazer.h"
+#include "pqueue.h"
+#include <queue>
+#include <limits>
 // TODO: include any other headers you need; remove this comment
 using namespace std;
 
 vector<Node *> depthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
     vector<Vertex*> path;
+    /*start->visited = true;
+    if(graph.compare(start, end)){
+        return path;
+    }
+    for(auto neighbour : graph.getNeighbors(start)){
+        if(!neighbour->visited){
+            path.push_back(neigbour);
+            depthFirstSearch(graph, neighbour, end);
+
+        }
+    }*/
     return path;
 }
 
@@ -19,11 +33,42 @@ vector<Node *> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end)
 }
 
 vector<Node *> dijkstrasAlgorithm(BasicGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
     vector<Vertex*> path;
+    vector<Vertex*> tmpPath;
+    PriorityQueue<Node *> que;
+    Set<Node *> vertexSet = graph.getVertexSet();
+    for(auto vertex : vertexSet){
+        if(!graph.compare(start, vertex)){
+            vertex->cost = numeric_limits<double>::max();
+        }
+        que.enqueue(vertex, vertex->cost);
+        Node* myNode = graph.getNode(vertex->name);
+        myNode->setColor(YELLOW);
+    }
+
+    while(!que.isEmpty()){
+        Vertex *u = que.dequeue();
+
+        if(graph.compare(u, end)){
+            while(u->previous != nullptr){
+                tmpPath.push_back(u);
+                u = u->previous;
+            }
+            while(!tmpPath.empty()){
+                path.push_back(tmpPath.back());
+                tmpPath.pop_back();
+            }
+        }
+        for(auto neighbour : graph.getNeighbors(u)){
+            double altDist = u->cost + (graph.getEdge(u, neighbour)->cost);
+            if(altDist < neighbour->cost){
+                neighbour->cost = altDist;
+                neighbour->previous = u;
+                que.changePriority(neighbour, altDist);
+            }
+        }
+    }
+
     return path;
 }
 
