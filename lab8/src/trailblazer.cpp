e7437d49de6f14e3a5b17581b544b2d29f0145a9
Hampus Eriksson <hamer848@li3-3.ida.liu.se>
2016-12-07 15:10:19 +0100

    Started on lab 8

--- lab8/src/trailblazer.cpp
+++ lab8/src/trailblazer.cpp
@@ -9,19 +9,11 @@
 using namespace std;
 
 vector<Node *> depthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
     vector<Vertex*> path;
     return path;
 }
 
 vector<Node *> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
     vector<Vertex*> path;
     return path;
 }
