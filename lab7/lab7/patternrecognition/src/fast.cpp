20debbd9e2734f3797b234afd4ca64757e60f6f7
Hampus Eriksson <hamer848@li10-1.ida.liu.se>
2016-12-13 14:08:10 +0100

    stylish code yes

--- lab7/lab7/patternrecognition/src/fast.cpp
+++ lab7/lab7/patternrecognition/src/fast.cpp
@@ -1,3 +1,9 @@
+/*
+ * TDDD86 Pattern Recognition
+ * This program computes and plots all line segments involving 4 points
+ * in a file using Qt. While doing executing the same task as brute.cpp
+ * fast does this way faster.
+ */
 
 #include <QApplication>
 #include <QGraphicsView>
@@ -16,22 +22,35 @@ using namespace std;
 static const int SCENE_WIDTH = 512;
 static const int SCENE_HEIGHT = 512;
 
+/*
+ * Draws specified dot on given scene object
+ */
 void render_points(QGraphicsScene* scene, const vector<Point>& points) {
     for(const auto& point : points) {
         point.draw(scene);
     }
 }
 
+/*
+ * Draws a line between two specified points on scene.
+ */
 void render_line(QGraphicsScene* scene, const Point& p1, const Point& p2) {
     p1.lineTo(scene, p2);
 }
 
-void cool_sort(vector<Point> points, const Point& oP, QGraphicsScene* scene){
+
+/*
+ * A very cool sorting function which firsts puts all points with the same
+ * slope to a given point in the same "bucket". The buckets containing more
+ * than 3 points are sorted and finally a line is drawn between them and
+ * the starting point.
+ */
+void cool_sort(vector<Point> points, const Point& orgPnt, QGraphicsScene* scene){
     map<double, vector<Point>> bucket;
 
     for(Point p : points){
-        if(oP < p || oP > p){
-            bucket[oP.slopeTo(p)].push_back(p);
+        if(orgPnt < p || orgPnt > p){
+            bucket[orgPnt.slopeTo(p)].push_back(p);
         }
     }
 
@@ -39,12 +58,16 @@ void cool_sort(vector<Point> points, const Point& oP, QGraphicsScene* scene){
         if(key.second.size() >= 3){
             vector<Point> tmpVec = key.second;
             sort(tmpVec.begin(), tmpVec.end());
-            render_line(scene, oP, tmpVec.back());
+            render_line(scene, orgPnt, tmpVec.back());
         }
     }
 
 }
 
+
+/*
+ * Main function
+ */
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
@@ -73,6 +96,7 @@ int main(int argc, char *argv[]) {
     QGraphicsView *view = new QGraphicsView();
     QGraphicsScene *scene = new QGraphicsScene(0, 0, SCENE_WIDTH, SCENE_HEIGHT);
     view->setScene(scene);
+
     // draw points to screen all at once
     render_points(scene, points);
     view->scale(1, -1); //screen y-axis is inverted
@@ -83,6 +107,8 @@ int main(int argc, char *argv[]) {
     // sort points by natural order
     // makes finding endpoints of line segments easy
     sort(points.begin(), points.end());
+
+    // create watch to count time taken to compute
     auto begin = chrono::high_resolution_clock::now();
     for(auto const &p : points){
         cool_sort(points, p, scene);
